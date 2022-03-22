98204e4cfdc7ba2e178884f0244bf20160fbd07e
toem1208 <101071945+toem1208@users.noreply.github.com>
2022-03-23 10:58:11 +1300

    area

---
+++ 03_perim_area_calculator.py
@@ -0,0 +1,41 @@
+# functions go here 
+
+# checks input is a number more than zero
+from turtle import width
+
+
+def num_check(question):
+    valid = False
+    while not valid:
+
+        error = "Please enter a number that is more than zero" 
+
+        try:
+
+            # ask user to enter a number 
+            response = float(input(question))
+
+            # checks number is more than zero
+            if response > 0:
+                return response
+
+            # outputs error if input is invalid
+            else:
+                    print(error)
+                    print()
+
+        except ValueError:
+            print(error)
+# Main Routine goes here 
+width = num_check("Width: ")
+height = num_check("Height: ")
+
+# Calculate are (width x height)
+area = width * height
+
+# Calaculate area (width + height) x 2
+perimeter = 2 * (width + height)
+
+# Output area and perimeter
+print("Perimeter: {} units".format(perimeter))
+print("Area: {} square units".format(area))
